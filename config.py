16489e1fcaa0c5cff7481fedeaddf0b66ef9bca0
Andrey <andrey@zavrichko.dev>
2024-08-22 20:22:09 +0300

    hw_20

--- config.py
+++ config.py
@@ -9,18 +9,16 @@ def to_driver_options(context):
     options = UiAutomator2Options()
 
     if context == 'local_emulator':
-        options.set_capability('remote_url', os.getenv('REMOTE_URL'))  # адрес удаленного сервера
-        options.set_capability('deviceName', os.getenv('DEVICE_NAME'))  # имя устройства
+        options.set_capability('remote_url', os.getenv('REMOTE_URL'))
+        options.set_capability('deviceName', os.getenv('DEVICE_NAME'))
         options.set_capability('appWaitActivity', os.getenv(
-            'APP_WAIT_ACTIVITY'))  # активити, которая будет открыта после запуска apk файла
-        # options.set_capability('udid', os.getenv('UDID')) # уникальный идентификатор устройства
+            'APP_WAIT_ACTIVITY'))
         options.set_capability('app', selene_in_action.utils.file.abs_path_from_project(os.getenv('APP')))
 
     if context == 'local_real_device':
         options.set_capability('remote_url', os.getenv('REMOTE_URL'))
         options.set_capability('deviceName', os.getenv('DEVICE_NAME'))
         options.set_capability('appWaitActivity', os.getenv('APP_WAIT_ACTIVITY'))
-        # options.set_capability('udid', os.getenv('UDID'))
         options.set_capability('app', selene_in_action.utils.file.abs_path_from_project(os.getenv('APP')))
 
     if context == 'bstack':
@@ -31,7 +29,7 @@ def to_driver_options(context):
         options.set_capability('appWaitActivity', os.getenv('APP_WAIT_ACTIVITY'))
         options.set_capability('app', os.getenv('APP'))
         load_dotenv(dotenv_path=selene_in_action.utils.file.abs_path_from_project(
-            '.env.credentials'))  # загрузка переменных окружения из файла .env.credentials
+            '.env.credentials'))
         options.set_capability(
             'bstack:options', {
                 'projectName': 'Wikipedia project',
